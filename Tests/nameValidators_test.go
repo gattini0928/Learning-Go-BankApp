b9532047a77a855fe11a7b0477b3ba8335566fe2
gattini0928 <gabrielgattini659@gmail.com>
2026-02-15 18:16:09 -0300

    corrigindo bugs nos validadores de senha e nome, criando primeiro testcase(nome)

---
+++ Tests/nameValidators_test.go
@@ -0,0 +1,55 @@
+package tests
+
+import ("testing"
+	"bankapp/validators"
+)
+
+func TestValidateName(t *testing.T) {
+	testCases := []struct {
+		name    string
+		input   string
+		wantMsg string
+		wantOk  bool
+	}{
+		{
+			name:    "Only First Name",
+			input:   "Gabriel",
+			wantMsg: "Por favor digite seu nome completo.",
+			wantOk:  false,
+		},
+		{
+			name:    "First Name Minimum Length",
+			input:   "Ga Gattini",
+			wantMsg: "Seu primeiro nome precisa ter pelo menos 3 letras.",
+			wantOk:  false,
+		},
+		{
+			name:    "Same Name and Surname",
+			input:   "Gabriel Gabriel",
+			wantMsg: "O nome e sobrenome não podem ser iguais",
+			wantOk:  false,
+		},
+		{
+			name:    "Digits in the Names",
+			input:   "Gabriel1 Gattini",
+			wantMsg: "Seu nome só pode conter letras",
+			wantOk:  false,
+		},
+		{
+			name:    "Full Name",
+			input:   "Gabriel Gattini",
+			wantMsg: "Nome está correto",
+			wantOk:  true,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			gotMsg, gotOk := validators.ValidateName(tt.input)
+			if gotMsg != tt.wantMsg || gotOk != tt.wantOk {
+				t.Fatalf("Esperava msg: %v, esperava ok: %v, mas recebeu msg: %v, ok: %v",
+					tt.wantMsg, tt.wantOk, gotMsg, gotOk)
+			}
+		})
+	}
+}
